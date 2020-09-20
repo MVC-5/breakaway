9bc310fe7cd3058b1b428dde54a6a7ef666e33ef
Josh Zappone <zappone.josh@gmail.com>
2020-09-20 16:53:02 -0700

    index hb temp done, request temp started

---
+++ public/assets/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  // $('.r-start').flatpickr();
+  flatpickr('.r-start', {});
+  flatpickr('.r-end', {});
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Requiring necessary npm packages
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -10,6 +11,17 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+app.get('/request', (req, res) => {
+  res.render('request');
+});
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({ force: true }).then(() => {
