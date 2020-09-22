daf99c17ee9bf76659fefa97307bfc0c624d0f00
Josh Zappone <zappone.josh@gmail.com>
2020-09-22 16:13:20 -0700

    request form creates new db request, eslint fix

--- controllers/manager_controller.js
+++ controllers/manager_controller.js
@@ -1,1 +1,2 @@
 // routes for the manager page and queries for data
+// if manager approves it should subtract from bank
--- controllers/request_controller.js
+++ controllers/request_controller.js
@@ -1,6 +1,18 @@
 // controller for making and viewing requests by employee id. also employee verification
 const db = require('../models');
 
+function getDate() {
+  const d = new Date();
+  let month = `${d.getMonth() + 1}`;
+  let day = `${d.getDate()}`;
+  const year = d.getFullYear();
+
+  if (month.length < 2) { month = `0${month}`; }
+  if (day.length < 2) { day = `0${day}`; }
+
+  return [year, month, day].join('-');
+}
+
 module.exports = function (app) {
   // login before viewing/making requests
   app.get('/api/login', (req, res) => {
@@ -10,13 +22,15 @@ module.exports = function (app) {
   });
 
   app.get('/request/:id', (req, res) => {
+    const employeeId = req.params.id;
     db.employee.findAll({
-      where: { id: req.params.id },
+      where: { id: employeeId },
       include: [{ model: db.request }],
+      order: [[db.request, 'start', 'DESC']],
     })
       .then((data) => {
         if (!data.length) {
-          const msg = { msg: `Employe id ${req.params.id} not found` };
+          const msg = { msg: `Employe id ${employeeId} not found` };
           res.status(404) // HTTP status 404: NotFound
             .render('404', msg);
         } else {
@@ -32,16 +46,40 @@ module.exports = function (app) {
             if (status === null) {
               status = 'pending';
             }
-            upcomingRequests.push({
-              dateCreated, startDate, endDate, status,
-            });
+            if (endDate >= getDate()) {
+              // only show dates that are upcoming
+              console.log(endDate);
+              upcomingRequests.push({
+                dateCreated, startDate, endDate, status,
+              });
+            }
           });
           // res.json(upcomingRequests);
-          res.render('request', { name: fName, upcomingRequests, bank });
+          res.render('request', {
+            name: fName, upcomingRequests, bank, id: employeeId,
+          });
         }
       })
       .catch((err) => {
         console.log(err);
+      });
+  });
+
+  app.post('/api/request', (req, res) => {
+    const d = req.body;
+    db.request.create({
+      start: d.formData.startDate,
+      end: d.formData.endDate,
+      reason: d.formData.reason,
+      employeeId: d.id,
+    })
+      .then((result) => {
+        console.log(result);
+        res.redirect('back');
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(404).send({ error: 'Something is wrong' });
       });
   });
 };
