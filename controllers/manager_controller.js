8a0f85a44028e6858e68e96aec17372c8d4baa8b
ShepLT1 <lshepherd234@gmail.com>
2020-09-23 10:10:09 -0700

    manager controller functions

--- controllers/manager_controller.js
+++ controllers/manager_controller.js
@@ -12,46 +12,35 @@ module.exports = (app) => {
   app.get('/manager/:id', (req, res) => {
     db.employee.findAll({
       where: { manager_id: req.params.id },
-      include: [{ model: }]
+      include: [{ model: db.request }],
     })
       .then((data) => {
-        let employeeRequests;
-        if (!data.length) {
-          const msg = { msg: `Either manager id ${req.params.id} not found or no employees are assigned to this manager` };
-          res.status(404) // HTTP status 404: NotFound
-            .render('404', msg);
-        } else {
-          console.log(data[0].dataValues.id);
-          employeeRequests = [];
-          for (let i = 0; i < data.length; i += i) {
-            console.log('yes');
-            db.request.findAll({
-              where: { employeeId: data[i].dataValues.id },
-            }).then((response) => {
-              const d = response.dataValues;
-              const employee = d.employeeId;
-              const { start } = d;
-              const { end } = d;
-              const requested = d.createdAt;
-              let { reason } = d;
-              let status = d.approved;
-              if (reason === null) {
-                reason = 'N/A';
-              }
-              if (status === null) {
-                status = 'Pending';
-              } else if (status === 0) {
-                status = 'Denied';
-              } else if (status === 1) {
-                status = 'Approved';
-              }
-              employeeRequests.push({
-                employee, start, end, requested, reason, status,
-              });
-            });
-          }
-          console.log(employeeRequests);
+        console.log(data);
+
+        const employeeRequests = [];
+
+        const d = data.dataValues;
+        const employee = d.employeeId;
+        const { start } = d;
+        const { end } = d;
+        const requested = d.createdAt;
+        let { reason } = d;
+        let status = d.approved;
+        if (reason === null) {
+          reason = 'N/A';
         }
+        if (status === null) {
+          status = 'Pending';
+        } else if (status === 0) {
+          status = 'Denied';
+        } else if (status === 1) {
+          status = 'Approved';
+        }
+        employeeRequests.push({
+          employee, start, end, requested, reason, status,
+        });
+
+        console.log(employeeRequests);
 
         // res.json(upcomingRequests);
         res.render('manager', { requests: employeeRequests });
