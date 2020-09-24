62370818b0d48b8d9aaf4bf57ca0b0b6d92ede7a
Josh Zappone <zappone.josh@gmail.com>
2020-09-23 23:43:08 -0700

    fixes multiple requests bug

--- controllers/request_controller.js
+++ controllers/request_controller.js
@@ -1,4 +1,5 @@
 // controller for making and viewing requests by employee id. also employee verification
+const moment = require('moment');
 const db = require('../models');
 
 function getDate() {
@@ -81,11 +82,18 @@ module.exports = function (app) {
 
   app.post('/api/request', (req, res) => {
     const d = req.body;
+    const startTime = d.formData.startDate;
+    const endTime = d.formData.endDate;
+    const duration = moment
+      .duration(moment(endTime, 'YYYY/MM/DD')
+        .diff(moment(startTime, 'YYYY/MM/DD'))).asDays();
+    console.log(duration);
     db.request.create({
       start: d.formData.startDate,
       end: d.formData.endDate,
       reason: d.formData.reason,
       employeeId: d.id,
+      duration,
     })
       .then((result) => {
         console.log(result);
--- public/assets/js/request.js
+++ public/assets/js/request.js
@@ -37,17 +37,3 @@ $(document).ready(() => {
     }
   });
 });
-
-function formatDate() {
-  const d = new Date();
-  let month = `${d.getMonth() + 1}`;
-  let day = `${d.getDate()}`;
-  const year = d.getFullYear();
-
-  if (month.length < 2) { month = `0${month}`; }
-  if (day.length < 2) { day = `0${day}`; }
-
-  return [year, month, day].join('-');
-}
-
-console.log(formatDate());
