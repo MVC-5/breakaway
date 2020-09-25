0586b931ef3021f338b0a7b7df1b246da32ee54d
Jon Phoenix <jonathan.martb@gmail.com>
2020-09-25 00:33:34 -0700

    Adds approved days calendar

---
+++ public/assets/js/calendar.js
@@ -0,0 +1,47 @@
+// this loop creates a day
+function displayDays(startDay, howManyDays, approvedDays){
+	for (let i = startDay; i < howManyDays; i++) {
+		let day = moment()
+		console.log(day.add(i, 'days').format('YYYY-MM-DD'));
+		dayBooked = false
+		//check the newly created against all days booked already
+		for (let j = 0; j < approvedDays.length; j++) {
+			if (day.format('YYYY-MM-DD') == approvedDays[j]){
+				dayBooked = true
+			}
+		}
+		if(dayBooked){
+			var newDay = document.createElement('div')
+			newDay.setAttribute('class', 'days')
+			newDay.style.backgroundColor = 'var(--accent-color)'
+			newDay.innerText = day.format('MM/DD')
+			document.querySelector('#calendar').appendChild(newDay)
+		} else {
+			var newDay = document.createElement('div')
+			newDay.setAttribute('class', 'days available')
+			newDay.style.backgroundColor = 'var(--primary-color)'
+			newDay.innerText = day.format('MM/DD')
+			document.querySelector('#calendar').appendChild(newDay)
+		}
+	}
+}
+// displayDays(0, 15)
+var availableDays = document.querySelectorAll('.available')
+console.log(availableDays);
+
+
+$(document).ready(() => {
+    function showCalendar(req) {
+        $.ajax({
+          method: 'GET',
+          url: '/api/approvedrequests/2',
+          data: req,
+        })
+          .then((days) => {
+            console.log(days);
+			displayDays(0,30, days)
+          });
+    };
+    
+    showCalendar();
+});
