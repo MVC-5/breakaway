630e73743ee6f40f560df6b14de801c7d0824775
Josh Zappone <zappone.josh@gmail.com>
2020-09-25 19:32:49 -0700

    redoes calendar functionality

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,47 +1,66 @@
-// This loop creates a day
-function displayDays(startDay, howManyDays, approvedDays){
-	for (let i = startDay; i < howManyDays; i++) {
-		let day = moment()
-		console.log(day.add(i, 'days').format('YYYY-MM-DD'));
-		dayBooked = false
-		//Check the newly created against all days booked already
-		for (let j = 0; j < approvedDays.length; j++) {
-			if (day.format('YYYY-MM-DD') == approvedDays[j]){
-				dayBooked = true
-			}
-		}
-		if(dayBooked){
-			var newDay = document.createElement('div')
-			newDay.setAttribute('class', 'days')
-			newDay.style.backgroundColor = 'var(--accent-color)'
-			newDay.innerText = day.format('MM/DD')
-			document.querySelector('#calendar').appendChild(newDay)
-		} else {
-			var newDay = document.createElement('div')
-			newDay.setAttribute('class', 'days available')
-			newDay.style.backgroundColor = 'var(--primary-color)'
-			newDay.innerText = day.format('MM/DD')
-			document.querySelector('#calendar').appendChild(newDay)
-		}
-	}
+function makeCalendar(daysApproved) {
+  const calTable = $('.cal-table');
+  const calRow1 = $('<tr>', { id: 'cal-1', class: 'a' });
+  const calRow2 = $('<tr>', { id: 'cal-2', class: 'a' });
+  const calRow3 = $('<tr>', { id: 'cal-3', class: 'a' });
+  const calRow4 = $('<tr>', { id: 'cal-4', class: 'a' });
+  const calRow5 = $('<tr>', { id: 'cal-5', class: 'a' });
+
+  let rowCount = 0;
+  daysApproved.forEach((day) => {
+    if (rowCount < 7) {
+      calRow1.append(`<td class="cal-day t-${day.taken}">${moment(day.date, 'ddd-YYYY-MM-DD').format('DD')}</td>`);
+    } else if (rowCount < 14) {
+      calRow2.append(`<td class="cal-day t-${day.taken}">${moment(day.date, 'ddd-YYYY-MM-DD').format('DD')}</td>`);
+    } else if (rowCount < 21) {
+      calRow3.append(`<td class="cal-day t-${day.taken}">${moment(day.date, 'ddd-YYYY-MM-DD').format('DD')}</td>`);
+    } else if (rowCount < 28) {
+      calRow4.append(`<td class="cal-day t-${day.taken}">${moment(day.date, 'ddd-YYYY-MM-DD').format('DD')}</td>`);
+    } else {
+      calRow5.append(`<td class="cal-day t-${day.taken}">${moment(day.date, 'ddd-YYYY-MM-DD').format('DD')}</td>`);
+    }
+
+    // theCal.append(`<td class="cal-day d-${day.taken}">${moment(day.date).format('DD')}</td>`);
+    rowCount += 1;
+  });
+  calTable.append(calRow1, calRow2, calRow3, calRow4, calRow5);
+  // console.log(theCal);
 }
 
-var availableDays = document.querySelectorAll('.available')
-console.log(availableDays);
-
-
 $(document).ready(() => {
-    function showCalendar(req) {
-        $.ajax({
-          method: 'GET',
-          url: '/api/approvedrequests/2',
-          data: req,
-        })
-          .then((days) => {
-            console.log(days);
-			displayDays(0,30, days)
-          });
-    };
-    
-    showCalendar();
+  if (!$('.aRequest').length) {
+    $('.aHead').remove();
+  }
+
+  if (!$('.bRequest').length) {
+    $('.bHead').remove();
+  }
+
+  function showCalendar(req) {
+    const location = window.location.href;
+    if (location.includes('manager')) {
+      const currentMan = location.slice(location.lastIndexOf('/') + 1);
+      $.ajax({
+        method: 'GET',
+        url: `/api/approvedrequests/${currentMan}`,
+        data: req,
+      })
+        .then((days) => {
+          console.log(days);
+          makeCalendar(days);
+        });
+    } else {
+      const currentEmp = location.slice(location.lastIndexOf('/') + 1);
+      $.ajax({
+        method: 'GET',
+        url: `/api/approvedrequests/emp/${currentEmp}`,
+        data: req,
+      })
+        .then((days) => {
+          makeCalendar(days);
+        });
+    }
+  }
+
+  showCalendar();
 });
